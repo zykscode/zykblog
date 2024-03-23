0e0570243b3b9fbd8f7abad705be1a17433a9d7f
zykscode <zyskson@gmail.com>
2024-03-23 10:15:47 +0100

    first commit

---
+++ data/headerNavLinks.ts
@@ -0,0 +1,8 @@
+const headerNavLinks = [
+  { href: '/', title: 'Home' },
+  { href: '/blog', title: 'Blog' },
+  { href: '/portfolio', title: 'Portfolio' },
+  { href: '/about', title: 'About' },
+];
+
+export default headerNavLinks;
