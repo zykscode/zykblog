79f8032bcfe8aa75d78ba8daf30baeeba1e487af
zykscode <zyskson@gmail.com>
2024-03-23 10:09:57 +0100

    first commit

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -1,30 +1,186 @@
-import readingTime from 'reading-time'
 import { defineDocumentType, ComputedFields, makeSource } from 'contentlayer/source-files'
+import { writeFileSync } from 'fs'
+import readingTime from 'reading-time'
+import { slug } from 'github-slugger'
+import path from 'path'
+// Remark packages
+import remarkGfm from 'remark-gfm'
+import remarkMath from 'remark-math'
 import {
-    remarkExtractFrontmatter,
-    remarkCodeTitles,
-    remarkImgToJsx,
-    extractTocHeadings,
-  } from 'pliny/mdx-plugins/index.js'
-
+  remarkExtractFrontmatter,
+  remarkCodeTitles,
+  remarkImgToJsx,
+  extractTocHeadings,
+} from 'pliny/mdx-plugins/index.js'
+// Rehype packages
+import rehypeSlug from 'rehype-slug'
+import rehypeAutolinkHeadings from 'rehype-autolink-headings'
+import rehypeKatex from 'rehype-katex'
+import rehypeCitation from 'rehype-citation'
+import rehypePrismPlus from 'rehype-prism-plus'
+import rehypePresetMinify from 'rehype-preset-minify'
+import siteMetadata from './data/siteMetadata'
+import { MDXDocumentDate, allCoreContent, sortPosts } from 'pliny/utils/contentlayer.js'
 
 const root = process.cwd()
 const isProduction = process.env.NODE_ENV === 'production'
+const tagColors = [
+  'item-blue',
+  'item-orange',
+  'item-green',
+  'item-pink',
+  'item-brown',
+  'item-red',
+  'item-yellow',
+  'item-default',
+  'item-purple',
+  'item-gray',
+]
 
-const computedFields:ComputedFields = {
-    readTime: {type:'json', resolve:(doc)=> readingTime(doc.body.raw)},
-    slug:{
-        type:'string',
-        resolve:(doc) => doc._raw.flattenedPath.replace(/^.+?(\/)/, ''),
-    },
-    path: {
-        type: 'string',
-        resolve: (doc) => doc._raw.flattenedPath,
-      },
-      filePath: {
-        type: 'string',
-        resolve: (doc) => doc._raw.sourceFilePath,
-      },
-      toc: { type: 'string', resolve: (doc) => extractTocHeadings(doc.body.raw) },
-    
+const computedFields: ComputedFields = {
+  readTime: { type: 'json', resolve: (doc) => readingTime(doc.body.raw) },
+  slug: {
+    type: 'string',
+    resolve: (doc) => doc._raw.flattenedPath.replace(/^.+?(\/)/, ''),
+  },
+  path: {
+    type: 'string',
+    resolve: (doc) => doc._raw.flattenedPath,
+  },
+  filePath: {
+    type: 'string',
+    resolve: (doc) => doc._raw.sourceFilePath,
+  },
+  toc: { type: 'string', resolve: (doc) => extractTocHeadings(doc.body.raw) },
 }
+
+interface Tag {
+  name: string
+  color: string
+}
+
+interface BlogDocument {
+  tags: Tag[]
+  draft: boolean
+}
+
+function createTagCount(allBlogs: BlogDocument[]) {
+  const tagCount: Record<string, { name: string; color: string; count: number }> = {}
+  allBlogs.forEach((file) => {
+    if (file.tags && (!isProduction || file.draft !== true)) {
+      file.tags.forEach((tag) => {
+        const formattedTag = slug(tag.name)
+        if (formattedTag in tagCount) {
+          tagCount[formattedTag].count += 1
+        } else {
+          tagCount[formattedTag] = { name: tag.name, color: tag.color, count: 1 }
+        }
+      })
+    }
+  })
+  writeFileSync('./app/tag-data.json', JSON.stringify(tagCount))
+}
+
+function createSearchIndex(allBlogs: MDXDocumentDate[]) {
+  if (
+    siteMetadata?.search?.provider === 'kbar' &&
+    siteMetadata.search.kbarConfig.searchDocumentsPath
+  ) {
+    writeFileSync(
+      `public/${siteMetadata.search.kbarConfig.searchDocumentsPath}`,
+      JSON.stringify(allCoreContent(sortPosts(allBlogs)))
+    )
+    console.log('Local search index generated...')
+  }
+}
+
+export const Blog = defineDocumentType(() => ({
+  name: 'Blog',
+  filePathPattern: 'blog/**/*.mdx',
+  contentType: 'mdx',
+  fields: {
+    title: { type: 'string', required: true },
+    date: { type: 'date', required: true },
+    tags: {
+      type: 'list',
+      of: {
+        type: 'object',
+        fields: {
+          name: { type: 'string', required: true },
+          color: { type: 'string', required: true, options: tagColors },
+        },
+      },
+      default: [],
+    },
+    lastmod: { type: 'date' },
+    draft: { type: 'boolean' },
+    summary: { type: 'string' },
+    images: { type: 'json' },
+    authors: { type: 'list', of: { type: 'string' } },
+    layout: { type: 'string' },
+    bibliography: { type: 'string' },
+    canonicalUrl: { type: 'string' },
+  },
+  computedFields: {
+    ...computedFields,
+    structuredData: {
+      type: 'json',
+      resolve: (doc) => ({
+        '@context': 'https://schema.org',
+        '@type': 'BlogPosting',
+        headline: doc.title,
+        datePublished: doc.date,
+        dateModified: doc.lastmod || doc.date,
+        description: doc.summary,
+        image: doc.images ? doc.images[0] : siteMetadata.socialBanner,
+        url: `${siteMetadata.siteUrl}/${doc._raw.flattenedPath}`,
+      }),
+    },
+  },
+}))
+
+export const Authors = defineDocumentType(() => ({
+  name: 'Authors',
+  filePathPattern: 'authors/**/*.mdx',
+  contentType: 'mdx',
+  fields: {
+    name: { type: 'string', required: true },
+    avatar: { type: 'string' },
+    occupation: { type: 'string' },
+    company: { type: 'string' },
+    email: { type: 'string' },
+    twitter: { type: 'string' },
+    linkedin: { type: 'string' },
+    github: { type: 'string' },
+    layout: { type: 'string' },
+  },
+  computedFields,
+}))
+
+export default makeSource({
+  contentDirPath: 'data',
+  documentTypes: [Blog, Authors],
+  mdx: {
+    cwd: process.cwd(),
+    remarkPlugins: [
+      remarkExtractFrontmatter,
+      remarkGfm,
+      remarkCodeTitles,
+      remarkMath,
+      remarkImgToJsx,
+    ],
+    rehypePlugins: [
+      rehypeSlug,
+      rehypeAutolinkHeadings,
+      rehypeKatex,
+      [rehypeCitation, { path: path.join(root, 'data') }],
+      [rehypePrismPlus, { defaultLanguage: 'js', ignoreMissing: true }],
+      rehypePresetMinify,
+    ],
+  },
+  onSuccess: async (importData) => {
+    const { allBlogs } = await importData()
+    createTagCount(allBlogs)
+    createSearchIndex(allBlogs)
+  },
+})
